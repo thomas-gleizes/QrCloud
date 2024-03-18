e45a5878c13e3d38916dce353844824da5bd481a
Thomas <thomasgleizes34@gmail.com>
2024-03-18 01:34:23 +0100

    init project

---
+++ src/main/java/org/tamikalat/qrcloud/QrCloudApplication.java
@@ -0,0 +1,13 @@
+package org.tamikalat.qrcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tamikalat/qrcloud/QrCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tamikalat.qrcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

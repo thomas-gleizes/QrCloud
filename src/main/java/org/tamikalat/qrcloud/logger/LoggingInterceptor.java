0a1d20cba60f29fd31a9c13838706393b46ee777
Thomas <thomasgleizes34@gmail.com>
2024-03-20 13:32:28 +0100

    refactor(back): use Spring logger

--- src/main/java/org/tamikalat/qrcloud/logger/LoggingInterceptor.java
+++ src/main/java/org/tamikalat/qrcloud/logger/LoggingInterceptor.java
@@ -2,18 +2,22 @@ package org.tamikalat.qrcloud.logger;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class LoggingInterceptor implements HandlerInterceptor {
+
+  Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
 
-    System.out.println(request.getMethod() + " " + request.getRequestURI());
+    logger.info(request.getMethod() + " " + request.getRequestURI());
 
     return true;
   }
